8fedb67d3f5654345467fd3cbcf36a316790bdc4
Marat-Akhalov <117514563+Marat-Akhalov@users.noreply.github.com>
2024-02-20 12:59:51 +0400

    feat: add base card component

---
+++ src/components/ui/BaseCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  padding: 20px;
+  border-radius: 10px;
+  border: 2px solid var(--clr-prime);
+}
+</style>
